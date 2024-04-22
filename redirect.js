6875924e4b9d02b1e0087a20adbf443a79ef02fe
AshuBotz <167718300+AshuBotz@users.noreply.github.com>
2024-04-23 02:39:25 +0530

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -12,31 +12,29 @@ function parseParams(url) {
     return params;
 }
 
-// Function to convert token URL to UPI format
-function convertTokenToUPI(token) {
-    var params = parseParams(token);
+// Function to convert parameters to UPI format
+function convertToUPIFormat(params) {
     var upi = params['u'];
     var name = params['n'];
     var amount = params['a'];
     var description = params['d'];
-    var upiFormat = `pa=${upi}&pn=${name}&am=${amount}&tn=${description}&cu=INR`;
+    var upiFormat = `upi://pay?pa=${upi}&pn=${name}&am=${amount}&tn=${description}&cu=INR`;
     return upiFormat;
 }
 
 // Function to redirect after 1 second
 function redirectWithDelay(upi) {
     setTimeout(function() {
-        window.location.href = `upi://pay?${upi}`;
+        window.location.href = upi;
     }, 1000);
 }
 
-// Extracting token from the current URL
+// Extracting parameters from the current URL
 var currentURL = window.location.href;
-var tokenIndex = currentURL.indexOf('?');
-var token = currentURL.substring(tokenIndex + 1);
+var params = parseParams(currentURL);
 
-// Converting token to UPI format
-var upiFormat = convertTokenToUPI(token);
+// Converting parameters to UPI format
+var upiFormat = convertToUPIFormat(params);
 
 // Redirecting after 1 second
 redirectWithDelay(upiFormat);
